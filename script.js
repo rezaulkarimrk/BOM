e340c2a05d806fdfb97a6be896a6f0c4ac099b07
rezaulkarimrk <mrk99rk@gmail.com>
2024-06-26 15:05:34 -0600

    finish

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/*
 let myWindow;
 const width = document.getElementById('width');
 const height = document.getElementById('height');
@@ -14,9 +14,9 @@ const closeWindow = () => {
     console.log('Hello')
     myWindow.close();
 };
+*/
 
-
-
+/*
 // select DOM Element
 const widthh = document.getElementById('width');
 const heightt = document.getElementById('height');
@@ -35,8 +35,9 @@ avaiHeight.innerHTML ="screen avaiHeight: " + screen.availHeight;
 
 pixelDepth.innerHTML = "screen pixelDepth: " + screen.pixelDepth;
 colorDepth.innerHTML = "screen colorDepth: " + screen.colorDepth;
+*/
 
-
+/*
 // select DOM Element
 const href = document.getElementById('href');
 const hostname = document.getElementById('hostname');
@@ -68,8 +69,11 @@ const userAgent = document.getElementById('userAgent');
 const language = document.getElementById('language');
 const onLine = document.getElementById('onLine');
 const javaEnabled = document.getElementById('javaEnabled');
+*/
+
 
 //show window.navigator object property values
+/*
 appName.innerHTML = "Navigator AppName: " + navigator.appName;
 appCodeName.innerHTML = "Navigator appCodeName: " + navigator.appCodeName;
 platform.innerHTML = "Navigator platform: " + navigator.platform;
@@ -80,3 +84,45 @@ userAgent.innerHTML = "Navigator userAgent: " + navigator.userAgent;
 language.innerHTML = "Navigator language: " + navigator.language;
 onLine.innerHTML = "Navigator onLine: " + navigator.onLine;
 javaEnabled.innerHTML = "Navigator javaEnabled: " + navigator.javaEnabled();
+*/
+
+
+const setCookie = (cName, cValue, exDays) => {
+    const d = new Date();
+    d.setTime(d.getTime() + exDays * 24 * 60 * 60 * 1000);
+
+    let expires = "expires="+ d.toString();
+
+    document.cookie = `${cName}=${cValue}; ${expires}; path=/`
+}
+
+const getCookie = (cname) => {
+    let name = cname + '='
+    let ca = document.cookie.split(';');
+    for(let i = 0; i< ca.length; i++){
+        let c = ca[i];
+        
+        if(c[0] === " "){
+            c = c.substring(1);
+        }
+
+        if(c.indexOf(name) === 0){
+            console.log(c.substring(name.length));
+            return c.substring(name.length);
+        }
+    }
+    return ""
+}
+
+const checkCookie = (cname, exDays=5) => {
+    let a = getCookie(cname);
+    console.log(a)
+    if(a != ""){
+        alert(`Welcome again: ${a}`);
+    }else{
+        name = prompt('Please Enter your name: ', "");
+        if(name != "" && name != null){
+            setCookie(cname, name, exDays);
+        }
+    }
+}
